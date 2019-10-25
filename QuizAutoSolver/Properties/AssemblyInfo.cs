6ca74bc2034f32ff0fd1ddb0a937ab14bf81d6eb
Raul Merelli <raul.merelli@hotmail.it>
2019-10-25 17:15:28 +0200

    Add files via upload

---
+++ QuizAutoSolver/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyConfiguration("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyTitle("Subnet quiz solver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("2018 - Merelli Raul")]
+[assembly: Guid("6818afec-8381-4158-92e6-10f80cbc4c19")]
+[assembly: AssemblyProduct("Subnet quiz solver")]
+[assembly: AssemblyCompany("Merelli Raul")]
+[assembly: AssemblyTrademark("Merelli Raul")]
+[assembly: ComVisible(false)]
